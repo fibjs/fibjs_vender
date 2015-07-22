c9e8d8f8249cee187a9dedb2b824578dcb8c12d4
响马 <lion@9465.net>
2015-07-22 18:38:36 +0800

    修复 windows 下 v8 编译错误

--- v8/src/base/platform/mutex.h
+++ v8/src/base/platform/mutex.h
@@ -11,6 +11,117 @@
 #endif
 
 namespace v8 {
+
+#ifdef _WIN32
+
+namespace internal {
+
+inline void unsupport(const char * fun)
+{
+  printf("%s not support!!!!!!!!!!!!\n", fun);
+  _exit(-1);
+}
+
+#ifndef InterlockedExchange8
+inline char InterlockedExchange8(char volatile *Target, char Value)
+{
+  unsupport("InterlockedExchange8");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedAnd8
+inline char InterlockedAnd8(char volatile *Target, char Value)
+{
+  unsupport("InterlockedAnd8");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedOr8
+inline char InterlockedOr8(char volatile *Target, char Value)
+{
+  unsupport("InterlockedOr8");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedXor8
+inline char InterlockedXor8(char volatile *Target, char Value)
+{
+  unsupport("InterlockedXor8");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedExchange16
+inline short InterlockedExchange16(short volatile *Target, short Value)
+{
+  unsupport("InterlockedExchange16");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedXor16
+inline short InterlockedXor16(short volatile *Target, short Value)
+{
+  unsupport("InterlockedXor16");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedExchange64
+inline int64_t InterlockedExchange64(int64_t volatile *Target, int64_t Value)
+{
+  unsupport("InterlockedExchange64");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedCompareExchange64
+inline int64_t InterlockedCompareExchange64(int64_t volatile *Target, int64_t Value, int64_t Old)
+{
+  unsupport("InterlockedCompareExchange64");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedAnd64
+inline int64_t InterlockedAnd64(int64_t volatile *Target, int64_t Value)
+{
+  unsupport("InterlockedAnd64");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedOr64
+inline int64_t InterlockedOr64(int64_t volatile *Target, int64_t Value)
+{
+  unsupport("InterlockedOr64");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedXor64
+inline int64_t InterlockedXor64(int64_t volatile *Target, int64_t Value)
+{
+  unsupport("InterlockedXor64");
+  return 0;
+}
+#endif
+
+#ifndef InterlockedExchangeAdd64
+inline int64_t InterlockedExchangeAdd64(int64_t volatile *Target, int64_t Value)
+{
+  unsupport("InterlockedExchangeAdd64");
+  return 0;
+}
+#endif
+
+}
+#endif
+
+
 namespace base {
 
 class Mutex final {
