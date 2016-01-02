84eedaa84e402e6e4e8bf86f9b02f931c45135d6
响马 <lion@9465.net>
2016-01-03 03:19:31 +0800

    修复 windows 32bits 编译警告

--- umysql/src/Connection.cpp
+++ umysql/src/Connection.cpp
@@ -611,7 +611,7 @@ void *Connection::handleResultPacket(int _fieldCount)
 
   // Read Field packets
 
-  UMTypeInfo *typeInfo = (UMTypeInfo *) alloca(fieldCount * sizeof(UMTypeInfo));
+  UMTypeInfo *typeInfo = (UMTypeInfo *) alloca((size_t)(fieldCount * sizeof(UMTypeInfo)));
   int iTypeInfo = 0;
 
   while (true)
