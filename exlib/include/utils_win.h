cacdfce035d38e00b7194218bbebeda3f5f3a0f7
响马 <lion@9465.net>
2016-07-18 03:08:19 +0800

    修复 inline 定义错误

--- exlib/include/utils_win.h
+++ exlib/include/utils_win.h
@@ -32,38 +32,6 @@ inline void yield()
 	YieldProcessor();
 }
 
-intptr_t CompareAndSwap(volatile intptr_t *ptr, intptr_t old_value, intptr_t new_value);
-
-inline void *_CompareAndSwap(void *volatile *ptr, void *old_value, void *new_value)
-{
-	return InterlockedCompareExchangePointer(ptr, new_value, old_value);
-}
-
-template<typename T>
-inline T *CompareAndSwap(T *volatile *ptr, T *old_value, T *new_value)
-{
-	void *result = _CompareAndSwap((void *volatile *)ptr, (void *)old_value, (void *)new_value);
-	return (T *)result;
-}
-
-inline void *CompareAndSwap(void *volatile *ptr, void *old_value, void *new_value)
-{
-	return _CompareAndSwap(ptr, old_value, new_value);
-}
-
-void *_atom_xchg(void *volatile *ptr, void *new_value);
-
-template<typename T>
-inline T *atom_xchg(T *volatile *ptr, T *new_value)
-{
-	void *result = _atom_xchg((void *volatile *)ptr, (void *)new_value);
-	return (T *)result;
-}
-
-inline void *atom_xchg(void *volatile *ptr, void *new_value)
-{
-	return _atom_xchg(ptr, new_value);
-}
 
 #ifdef amd64
 inline intptr_t CompareAndSwap(volatile intptr_t *ptr, intptr_t old_value, intptr_t new_value)
@@ -145,6 +113,37 @@ inline intptr_t atom_xchg(volatile intptr_t *ptr, intptr_t new_value)
 inline void *_atom_xchg(void *volatile *ptr, void *new_value)
 {
 	return InterlockedExchangePointer(ptr, new_value);
+}
+
+inline void *_CompareAndSwap(void *volatile *ptr, void *old_value, void *new_value)
+{
+	return InterlockedCompareExchangePointer(ptr, new_value, old_value);
+}
+
+template<typename T>
+inline T *CompareAndSwap(T *volatile *ptr, T *old_value, T *new_value)
+{
+	void *result = _CompareAndSwap((void *volatile *)ptr, (void *)old_value, (void *)new_value);
+	return (T *)result;
+}
+
+inline void *CompareAndSwap(void *volatile *ptr, void *old_value, void *new_value)
+{
+	return _CompareAndSwap(ptr, old_value, new_value);
+}
+
+void *_atom_xchg(void *volatile *ptr, void *new_value);
+
+template<typename T>
+inline T *atom_xchg(T *volatile *ptr, T *new_value)
+{
+	void *result = _atom_xchg((void *volatile *)ptr, (void *)new_value);
+	return (T *)result;
+}
+
+inline void *atom_xchg(void *volatile *ptr, void *new_value)
+{
+	return _atom_xchg(ptr, new_value);
 }
 
 }
