84382b2344e2026179c884110dccec2a67836828
响马 <lion@9465.net>
2016-07-25 19:30:56 +0800

    修复 v8 遗漏的补丁

--- v8/src/base/platform/semaphore.h
+++ v8/src/base/platform/semaphore.h
@@ -1,102 +1,65 @@
-// Copyright 2013 the V8 project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef V8_BASE_PLATFORM_SEMAPHORE_H_
-#define V8_BASE_PLATFORM_SEMAPHORE_H_
+#ifndef V8_PLATFORM_SEMAPHORE_H_
+#define V8_PLATFORM_SEMAPHORE_H_
 
 #include "src/base/lazy-instance.h"
+#include <exlib/include/fiber.h>
+
 #if V8_OS_WIN
 #include "src/base/win32-headers.h"
 #endif
 
-#if V8_OS_MACOSX
-#include <mach/semaphore.h>  // NOLINT
-#elif V8_OS_POSIX
-#include <semaphore.h>  // NOLINT
-#endif
+namespace v8
+{
+namespace base
+{
 
-namespace v8 {
-namespace base {
-
-// Forward declarations.
 class TimeDelta;
 
-// ----------------------------------------------------------------------------
-// Semaphore
-//
-// A semaphore object is a synchronization object that maintains a count. The
-// count is decremented each time a thread completes a wait for the semaphore
-// object and incremented each time a thread signals the semaphore. When the
-// count reaches zero,  threads waiting for the semaphore blocks until the
-// count becomes non-zero.
+class Semaphore final
+{
+public:
+    explicit Semaphore(int count);
+    ~Semaphore();
 
-class Semaphore final {
- public:
-  explicit Semaphore(int count);
-  ~Semaphore();
+    void Signal();
+    void Wait(); bool WaitFor(const TimeDelta &rel_time) WARN_UNUSED_RESULT;
 
-  // Increments the semaphore counter.
-  void Signal();
+    typedef exlib::Semaphore NativeHandle;
 
-  // Suspends the calling thread until the semaphore counter is non zero
-  // and then decrements the semaphore counter.
-  void Wait();
+    NativeHandle &native_handle()
+    {
+        return native_handle_;
+    }
+    const NativeHandle &native_handle() const
+    {
+        return native_handle_;
+    }
 
-  // Suspends the calling thread until the counter is non zero or the timeout
-  // time has passed. If timeout happens the return value is false and the
-  // counter is unchanged. Otherwise the semaphore counter is decremented and
-  // true is returned.
-  bool WaitFor(const TimeDelta& rel_time) WARN_UNUSED_RESULT;
+private:
+    NativeHandle native_handle_;
 
-#if V8_OS_MACOSX
-  typedef semaphore_t NativeHandle;
-#elif V8_OS_POSIX
-  typedef sem_t NativeHandle;
-#elif V8_OS_WIN
-  typedef HANDLE NativeHandle;
-#endif
-
-  NativeHandle& native_handle() {
-    return native_handle_;
-  }
-  const NativeHandle& native_handle() const {
-    return native_handle_;
-  }
-
- private:
-  NativeHandle native_handle_;
-
-  DISALLOW_COPY_AND_ASSIGN(Semaphore);
+    DISALLOW_COPY_AND_ASSIGN(Semaphore);
 };
 
-
-// POD Semaphore initialized lazily (i.e. the first time Pointer() is called).
-// Usage:
-//   // The following semaphore starts at 0.
-//   static LazySemaphore<0>::type my_semaphore = LAZY_SEMAPHORE_INITIALIZER;
-//
-//   void my_function() {
-//     // Do something with my_semaphore.Pointer().
-//   }
-//
-
-template <int N>
-struct CreateSemaphoreTrait {
-  static Semaphore* Create() {
-    return new Semaphore(N);
-  }
+template<int N>
+struct CreateSemaphoreTrait
+{
+    static Semaphore *Create()
+    {
+        return new Semaphore(N);
+    }
 };
 
-template <int N>
-struct LazySemaphore {
-  typedef typename LazyDynamicInstance<Semaphore, CreateSemaphoreTrait<N>,
-                                       ThreadSafeInitOnceTrait>::type type;
+template<int N>
+struct LazySemaphore
+{
+    typedef typename base::LazyDynamicInstance<Semaphore, CreateSemaphoreTrait<N>,
+            base::ThreadSafeInitOnceTrait>::type type;
 };
 
 #define LAZY_SEMAPHORE_INITIALIZER LAZY_DYNAMIC_INSTANCE_INITIALIZER
 
-}  // namespace base
-}  // namespace v8
+}
+}
 
-#endif  // V8_BASE_PLATFORM_SEMAPHORE_H_
+#endif  // V8_PLATFORM_SEMAPHORE_H_
