44d7f28c68b96031bc255f194939db836b714300
响马 <lion@9465.net>
2017-07-02 22:32:51 +0800

    remove icu and spidermonkey library.

--- jssdk/test/src/testmain.cpp
+++ jssdk/test/src/testmain.cpp
@@ -2,21 +2,21 @@
 #include "exlib/include/service.h"
 #include "jssdk.h"
 
-void *fiber_proc(void *p)
+void* fiber_proc(void* p)
 {
-	_exit(RUN_ALL_TESTS());
-	return 0;
+    _exit(RUN_ALL_TESTS());
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
-	testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
 
-	js::setup_v8();
-	js::setup_spider();
+    js::setup_v8();
+    // js::setup_spider();
 
-	exlib::Service::init(3);
-	exlib::Service::Create(fiber_proc, 0, 128 * 1024);
-	exlib::Service::dispatch();
-	return 0;
+    exlib::Service::init(3);
+    exlib::Service::Create(fiber_proc, 0, 128 * 1024);
+    exlib::Service::dispatch();
+    return 0;
 }
