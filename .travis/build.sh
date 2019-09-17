c7689c0d3cf58268c4ffb301ca5b79aa7a66eac5
richardo2016 <richardo2016@gmail.com>
2019-09-18 05:03:09 +0800

    ci, feat: use better release package format in travis.

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,8 +4,9 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "start docker build..."
-    DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
+    DIR=`pwd`;sudo docker run -it --env GIT_BRANCH=${TRAVIS_BRANCH} -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
 else
+    export GIT_BRANCH=${TRAVIS_BRANCH}
     sh build -j2 ${ARCH} ci
 fi
 
