8815b35d6c74c8e32d1283a7c9a2f77bcfe77a38
xicilion <lion@9465.net>
2019-10-19 20:32:34 +0800

    exlib, refactor: remove CondVar support for Windows XP.

--- exlib/include/thread.h
+++ exlib/include/thread.h
@@ -174,78 +174,6 @@ public:
     HANDLE m_sem;
 };
 
-class OSCondVarOld {
-public:
-    OSCondVarOld(OSMutex* user_lock);
-    ~OSCondVarOld();
-
-    void Wait()
-    {
-        TimedWait(INFINITE);
-    }
-
-    void TimedWait(DWORD dwMilliseconds);
-    void Signal();
-    void SignalAll();
-
-private:
-    class Event {
-    public:
-        Event();
-        ~Event();
-
-        void InitListElement();
-
-        bool IsEmpty() const;
-        void PushBack(Event* other);
-        Event* PopFront();
-        Event* PopBack();
-
-        HANDLE handle() const;
-        Event* Extract();
-
-        bool IsSingleton() const;
-
-    private:
-        bool ValidateAsDistinct(Event* other) const;
-        bool ValidateAsItem() const;
-        bool ValidateAsList() const;
-        bool ValidateLinks() const;
-
-        HANDLE handle_;
-        Event* next_;
-        Event* prev_;
-    };
-
-private:
-    enum RunState { SHUTDOWN = 0,
-        RUNNING = 64213 };
-
-    Event* GetEventForWaiting();
-    void RecycleEvent(Event* used_event);
-
-    RunState run_state_;
-    OSMutex internal_lock_;
-    OSMutex& user_lock_;
-    Event waiting_list_;
-    Event recycling_list_;
-    int32_t recycling_list_size_;
-    int32_t allocation_counter_;
-};
-
-class OSCondVarNew {
-public:
-    OSCondVarNew(OSMutex* mu);
-    ~OSCondVarNew();
-    void Wait();
-    void Signal();
-    void SignalAll();
-
-private:
-    CONDITION_VARIABLE _cv;
-    OSMutex* _mu;
-};
-
 class OSCondVar {
 public:
     OSCondVar(OSMutex* mu);
@@ -255,7 +183,8 @@ public:
     void SignalAll();
 
 private:
-    void* _cd;
+    CONDITION_VARIABLE _cv;
+    OSMutex* _mu;
 };
 
 #else
