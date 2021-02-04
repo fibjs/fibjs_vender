54698b67645d2054f48902703f796ff78247f9a0
richardo2016 <richardo2016@gmail.com>
2021-02-04 21:13:37 +0800

    build, feat: use cmake as build workflow driver.

--- tools/get_env.cmake
+++ tools/get_env.cmake
@@ -1,3 +1,37 @@
+# get host's architecture in cmake script mode
+function(gethostarch RETVAL)
+    if("${${RETVAL}}" STREQUAL "")
+        if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+            set(HOST_SYSTEM_PROCESSOR amd64)
+        else()
+            execute_process(
+                COMMAND uname -m
+                OUTPUT_VARIABLE HOST_SYSTEM_PROCESSOR
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+        endif()
+
+        if(${HOST_SYSTEM_PROCESSOR} MATCHES "^(i386)|(i686)|(x86)$")
+            set(${RETVAL} i386 PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^(x86_64)|(amd64)|(AMD64)$")
+            set(${RETVAL} amd64 PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^(armv6)|(armv7)|(armv7s)|(armv7l)$")
+            set(${RETVAL} arm PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^aarch64$")
+            set(${RETVAL} arm64 PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^(mips)|(mipsel)$")
+            set(${RETVAL} mips PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^mips64$")
+            set(${RETVAL} mips64 PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^powerpc$")
+            set(${RETVAL} ppc PARENT_SCOPE)
+        elseif(${HOST_SYSTEM_PROCESSOR} MATCHES "^ppc64$")
+            set(${RETVAL} ppc64 PARENT_SCOPE)
+        endif()
+    endif()
+endfunction()
+
+
 function(build src out)
     file(MAKE_DIRECTORY "${out}")
 
@@ -30,46 +64,26 @@ endfunction()
 
 include(ProcessorCount)
 
-if("${BUILD_ARCH}" STREQUAL "")
-    if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-        set(CMAKE_HOST_SYSTEM_PROCESSOR amd64)
-    else()
-        execute_process(COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE CMAKE_HOST_SYSTEM_PROCESSOR)
-    endif()
-
-    if(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^(i386)|(i686)|(x86)$")
-        set(BUILD_ARCH i386)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^(x86_64)|(amd64)|(AMD64)$")
-        set(BUILD_ARCH amd64)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^(armv6)|(armv7)|(armv7s)|(armv7l)$")
-        set(BUILD_ARCH arm)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^aarch64$")
-        set(BUILD_ARCH arm64)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^(mips)|(mipsel)$")
-        set(BUILD_ARCH mips)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^mips64$")
-        set(BUILD_ARCH mips64)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^powerpc$")
-        set(BUILD_ARCH ppc)
-    elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} MATCHES "^ppc64$")
-        set(BUILD_ARCH ppc64)
-    endif()
+gethostarch(HOST_ARCH)
+if(NOT DEFINED BUILD_ARCH OR "${BUILD_ARCH}" STREQUAL "")
+    gethostarch(BUILD_ARCH)
 endif()
-
-ProcessorCount(CMAKE_HOST_SYSTEM_PROCESSOR_COUNT)
 
 if("${BUILD_TYPE}" STREQUAL "")
     set(BUILD_TYPE release)
 endif()
 
 if("${BUILD_JOBS}" STREQUAL "")
+    ProcessorCount(CMAKE_HOST_SYSTEM_PROCESSOR_COUNT)
     set(BUILD_JOBS ${CMAKE_HOST_SYSTEM_PROCESSOR_COUNT})
 endif()
 
 set(ENV{CLICOLOR_FORCE} 1)
 
 message("")
+message("HOST_OS is ${CMAKE_HOST_SYSTEM_NAME}")
 message("BUILD_OS is ${CMAKE_HOST_SYSTEM_NAME}")
+message("HOST_ARCH is ${HOST_ARCH}")
 message("BUILD_ARCH is ${BUILD_ARCH}")
 message("BUILD_TYPE is ${BUILD_TYPE}")
 message("BUILD_JOBS is ${BUILD_JOBS}")
