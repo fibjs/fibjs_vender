ba33756a2b52b5d31274299397cb0351e1e7193b
richardo2016 <richardo2016@gmail.com>
2021-02-05 03:50:47 +0800

    build, feat: more friendly to external program.

--- build.cmake
+++ build.cmake
@@ -41,46 +41,28 @@ else()
 endif()
 
 if(EXISTS "../build")
-    set(OUT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../out/${CMAKE_HOST_SYSTEM_NAME}_${BUILD_ARCH}_${BUILD_TYPE}")
+    set(WORK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/..")
 else()
-    set(OUT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/.dist/out/${CMAKE_HOST_SYSTEM_NAME}_${BUILD_ARCH}_${BUILD_TYPE}")
+    set(WORK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/.dist")
 endif()
 
-if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-    # @todo set EnvVar to tell clang use VS2017 rather than newest one if it's not VS2017
-    #
-    # by default, clang/LLVM would find newest VCToolChain, if you install both VS2017 and VS2019
-    # VS2019's VCTools would be used to compile vender, but v8 6.9 cannot be compiled with it, we must
-    # tell clang to use VS2017's VCToolChain
-    if(NOT DEFINED VCToolsInstallDir)
-        if("$ENV{ProgramW6432}" STREQUAL "")
-            set(PROGRAM_FILES_X86 "$ENV{ProgramFiles\(x86\)}")
-        else()
-            set(PROGRAM_FILES_X86 "$ENV{ProgramW6432} (x86)")
+set(BIN_ROOT "${WORK_ROOT}/bin")
+set(OUT_ROOT "${WORK_ROOT}/out")
+set(DIST_DIRNAME "${CMAKE_HOST_SYSTEM_NAME}_${BUILD_ARCH}_${BUILD_TYPE}")
+
+if("${CLEAN_BUILD}" STREQUAL "true")
+    rimraf(${BIN_ROOT}/${DIST_DIRNAME})
+    rimraf(${OUT_ROOT}/${DIST_DIRNAME})
+else()
+    set(OUT_PATH "${OUT_ROOT}/${DIST_DIRNAME}")
+
+    foreach(lib ${libs})
+        build("${CMAKE_CURRENT_SOURCE_DIR}/${lib}" "${OUT_PATH}/${lib}")
+    endforeach()
+
+    foreach(lib ${libs})
+        if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${lib}/test")
+            build("${CMAKE_CURRENT_SOURCE_DIR}/${lib}/test" "${OUT_PATH}/${lib}_test")
         endif()
-        message("[win32] PROGRAM_FILES_X86 is ${PROGRAM_FILES_X86}")
-        
-        execute_process(
-            WORKING_DIRECTORY "${out}"
-            COMMAND "${PROGRAM_FILES_X86}\\Microsoft\ Visual\ Studio\\Installer\\vswhere.exe" -property installationPath -version "[15.0, 16.0)"
-            OUTPUT_VARIABLE VS_INSTALLPATH
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-
-        file(STRINGS "${VS_INSTALLPATH}\\VC\\Auxiliary\\Build\\Microsoft.VCToolsVersion.default.txt" CUR_MSVC_1900_VER)
-        message("[win32] CUR_MSVC_1900_VER is ${CUR_MSVC_1900_VER}")
-
-        set(ENV{VCToolsInstallDir} "${VS_INSTALLPATH}\\VC\\Tools\\MSVC\\${CUR_MSVC_1900_VER}")
-        message("[win32] ENV{VCToolsInstallDir} is $ENV{VCToolsInstallDir}")
-    endif()
+    endforeach()
 endif()
-
-foreach(lib ${libs})
-    build("${CMAKE_CURRENT_SOURCE_DIR}/${lib}" "${OUT_PATH}/${lib}")
-endforeach()
-
-foreach(lib ${libs})
-    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${lib}/test")
-        build("${CMAKE_CURRENT_SOURCE_DIR}/${lib}/test" "${OUT_PATH}/${lib}_test")
-    endif()
-endforeach()
--- tools/get_env.cmake
+++ tools/get_env.cmake
@@ -31,9 +31,16 @@ function(gethostarch RETVAL)
     endif()
 endfunction()
 
-
 function(build src out)
     file(MAKE_DIRECTORY "${out}")
+
+    if(NOT DEFINED HOST_ARCH)
+        message( FATAL_ERROR "[get_env::build] BUILD_TYPE haven't been set, check your input.")
+    endif()
+
+    if(NOT DEFINED HOST_ARCH)
+        message( FATAL_ERROR "[get_env::build] HOST_ARCH haven't been set, call `gethostarch(HOST_ARCH)` before `build()`")
+    endif()
 
     execute_process(WORKING_DIRECTORY "${out}"
         OUTPUT_FILE CMake.log 
@@ -43,27 +50,74 @@ function(build src out)
             -G "Unix Makefiles"
             -DCMAKE_C_COMPILER=clang
             -DCMAKE_CXX_COMPILER=clang++
+            -DHOST_ARCH=${HOST_ARCH}
             -DARCH=${BUILD_ARCH}
             -DBUILD_TYPE=${BUILD_TYPE}
             "${src}"
         RESULT_VARIABLE STATUS
+        ERROR_VARIABLE BUILD_ERROR
     )
 
     if(STATUS AND NOT STATUS EQUAL 0)
-        EXIT()
+        message("[get_env::build::error::cmake] for '${out}'")
+        message(FATAL_ERROR "${BUILD_ERROR}")
     endif()
 
     execute_process(WORKING_DIRECTORY "${out}"
-            COMMAND make -j${BUILD_JOBS}
-            RESULT_VARIABLE STATUS)
+        COMMAND make -j${BUILD_JOBS}
+        RESULT_VARIABLE STATUS
+        ERROR_VARIABLE BUILD_ERROR
+    )
 
     if(STATUS AND NOT STATUS EQUAL 0)
-        EXIT()
+        message("[get_env::build::error::make] for '${out}'")
+        message(FATAL_ERROR "${BUILD_ERROR}")
+    endif()
+endfunction()
+
+function(prepare_platform)
+    if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+        # @todo set EnvVar to tell clang use VS2017 rather than newest one if it's not VS2017
+        #
+        # by default, clang/LLVM would find newest VCToolChain, if you install both VS2017 and VS2019
+        # VS2019's VCTools would be used to compile vender, but v8 6.9 cannot be compiled with it, we must
+        # tell clang to use VS2017's VCToolChain
+        if(NOT DEFINED VCToolsInstallDir)
+            if("$ENV{ProgramW6432}" STREQUAL "")
+                set(PROGRAM_FILES_X86 "$ENV{ProgramFiles\(x86\)}")
+            else()
+                set(PROGRAM_FILES_X86 "$ENV{ProgramW6432} (x86)")
+            endif()
+            message("[win32] PROGRAM_FILES_X86 is ${PROGRAM_FILES_X86}")
+            
+            execute_process(
+                WORKING_DIRECTORY "${out}"
+                COMMAND "${PROGRAM_FILES_X86}\\Microsoft\ Visual\ Studio\\Installer\\vswhere.exe" -property installationPath -version "[15.0, 16.0)"
+                OUTPUT_VARIABLE VS_INSTALLPATH
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+
+            file(STRINGS "${VS_INSTALLPATH}\\VC\\Auxiliary\\Build\\Microsoft.VCToolsVersion.default.txt" CUR_MSVC_1900_VER)
+            message("[win32] CUR_MSVC_1900_VER is ${CUR_MSVC_1900_VER}")
+
+            set(ENV{VCToolsInstallDir} "${VS_INSTALLPATH}\\VC\\Tools\\MSVC\\${CUR_MSVC_1900_VER}")
+            message("[win32] ENV{VCToolsInstallDir} is $ENV{VCToolsInstallDir}")
+        endif()
+    endif()
+endfunction()
+
+function(rimraf TARGET)
+    if(EXISTS "${TARGET}")
+        file(REMOVE_RECURSE ${TARGET})
+        message("removed ${BIN_ROOT}/${DIST_DIRNAME}")
+    else()
+        message("path '${BIN_ROOT}/${DIST_DIRNAME}' didn't existed, no removal required.")
     endif()
 endfunction()
 
 include(ProcessorCount)
 
+prepare_platform()
 gethostarch(HOST_ARCH)
 if(NOT DEFINED BUILD_ARCH OR "${BUILD_ARCH}" STREQUAL "")
     gethostarch(BUILD_ARCH)
