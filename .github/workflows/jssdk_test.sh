a5d92cc0cde3c110839d5b589c3709ad2a6fc201
richardo2016 <richardo2016@gmail.com>
2021-03-16 19:00:11 +0800

    ci, feat: fixup error about running jssdk_test in arm/arm64

---
+++ .github/workflows/jssdk_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# run jssdk_test
+
+set -ev
+
+if [[ $TARGET_OS_NAME == 'Linux' ]]; then
+    if [[ $TARGET_ARCH == "arm" ]]; then
+        # run jssdk_test on arm using qemu
+        sudo docker run --rm --privileged multiarch/qemu-user-static:register
+        DIR=`pwd`;sudo docker run --privileged=true -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
+        cd /home;
+        sh init_armhf.sh;
+        cp -f ./ci/.dist/bin/Linux_arm_release/jssdk_test ./arm_root_fs/bin/jssdk_test;
+        chroot ./arm_root_fs jssdk_test"
+    elif [[ $TARGET_ARCH == "arm64" ]]; then
+        # run jssdk_test on arm64 using qemu
+        sudo docker run --rm --privileged multiarch/qemu-user-static:register
+        DIR=`pwd`;sudo docker run --privileged=true -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
+        cd /home;
+        sh init_arm64.sh;
+        cp -f ./ci/.dist/bin/Linux_arm64_release/jssdk_test ./arm64_root_fs/bin/jssdk_test;
+        chroot ./arm64_root_fs jssdk_test"
+    else
+        .dist/bin/$DIST_DIR/jssdk_test
+    fi
+else # Darwin/Windows
+    .dist/bin/$DIST_DIR/jssdk_test
+fi
+
+exit 0;
