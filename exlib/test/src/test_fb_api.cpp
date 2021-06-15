99cc3dfe371ed903663b80c47349bab4fda415c2
richardo2016 <richardo2016@gmail.com>
2021-06-16 02:58:41 +0800

    exlib, bugfix: fixup test about fb_api on Windows.

--- exlib/test/src/test_fb_api.cpp
+++ exlib/test/src/test_fb_api.cpp
@@ -1,16 +1,47 @@
 #include "gtest/gtest.h"
 #include "exlib/include/fb_api.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void deleteFiberAddress(void* curFiberAddress)
+{
+#ifndef _WIN32
+    exlib::delete_fiber(curFiberAddress);
+#endif
+}
+
 TEST(exlib_fb_api, convert_fiber)
 {
+#ifndef _WIN32
     // get current context info by `exlib::convert_fiber`
     void* ctx = exlib::convert_fiber(NULL);
+#else
+    // but for win32, we have been in one fiber from test context
+    void* ctx = GetCurrentFiber();
+#endif
     EXPECT_NE(ctx, nullptr);
-
-    exlib::delete_fiber(ctx);
+    deleteFiberAddress(ctx);
 
     // after deallocation, pointer's still here
     EXPECT_NE(ctx, nullptr);
+}
+
+void* convertFiberIfNotInFiber(void* param)
+{
+#ifndef _WIN32
+    return exlib::convert_fiber(param);
+#else
+    void* fiberAddr = GetCurrentFiber();
+    if (fiberAddr == NULL) {
+        int err_code = GetLastError();
+        printf("GetCurrentFiber error (%d)\n", err_code);
+        return exlib::convert_fiber(param);
+    }
+
+    return fiberAddr;
+#endif
 }
 
 static void* currentCtx = nullptr;
@@ -26,7 +57,7 @@ static void fiber_plusplus(void* p)
 TEST(exlib_fb_api, use_fiber)
 {
     int a = 0;
-    currentCtx = exlib::convert_fiber(NULL);
+    currentCtx = convertFiberIfNotInFiber(NULL);
     ctxToBeTested = exlib::create_fiber(128 * 1024, fiber_plusplus, &a);
 
     EXPECT_NE(ctxToBeTested, nullptr);
@@ -38,7 +69,7 @@ TEST(exlib_fb_api, use_fiber)
     // context switched, execution taken
     EXPECT_EQ(a, 1);
 
-    exlib::delete_fiber(currentCtx);
+    deleteFiberAddress(currentCtx);
     exlib::delete_fiber(ctxToBeTested);
 
     EXPECT_NE(currentCtx, nullptr);
