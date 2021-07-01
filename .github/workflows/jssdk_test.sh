d275d098f1fab604d0b1bf331a345c55b15064e3
richardo2016 <richardo2016@gmail.com>
2021-07-01 10:33:06 +0800

    ci, feat: run jssdk_test on debug also.

--- .github/workflows/jssdk_test.sh
+++ .github/workflows/jssdk_test.sh
@@ -11,7 +11,7 @@ if [[ $TARGET_OS_NAME == 'Linux' ]]; then
         DIR=`pwd`;sudo docker run --privileged=true -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
         cd /home;
         sh init_armhf.sh;
-        cp -f ./ci/.dist/bin/Linux_arm_release/jssdk_test ./arm_root_fs/bin/jssdk_test;
+        cp -f ./ci/.dist/bin/$DIST_DIR/jssdk_test ./arm_root_fs/bin/jssdk_test;
         chroot ./arm_root_fs jssdk_test"
     elif [[ $TARGET_ARCH == "arm64" ]]; then
         # run jssdk_test on arm64 using qemu
@@ -19,13 +19,13 @@ if [[ $TARGET_OS_NAME == 'Linux' ]]; then
         DIR=`pwd`;sudo docker run --privileged=true -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
         cd /home;
         sh init_arm64.sh;
-        cp -f ./ci/.dist/bin/Linux_arm64_release/jssdk_test ./arm64_root_fs/bin/jssdk_test;
+        cp -f ./ci/.dist/bin/$DIST_DIR/jssdk_test ./arm64_root_fs/bin/jssdk_test;
         chroot ./arm64_root_fs jssdk_test"
     elif [[ $TARGET_ARCH == "i386" ]]; then
         # libatomic.so.1 required, but not set in ubuntu-16.04/ubuntu-18.04 by default, so we run it in docker
         DIR=`pwd`;sudo docker run -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
         cd /home/ci;
-        ./.dist/bin/Linux_i386_release/jssdk_test
+        ./.dist/bin/$DIST_DIR/jssdk_test
         "
     else
         .dist/bin/$DIST_DIR/jssdk_test
