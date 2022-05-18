ff32a6f234d4d2b3b48a1b6a34483413a269b739
xicilion <lion@9465.net>
2022-05-18 23:15:59 +0800

    build, bugfix: github ci error.

--- .github/workflows/gen_snapshot.sh
+++ .github/workflows/gen_snapshot.sh
@@ -5,32 +5,7 @@
 set -ev
 
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
-    if [[ $TARGET_ARCH == "arm" ]]; then
-        # generate v8-snapshot on arm using qemu
-        sudo docker run --rm --privileged multiarch/qemu-user-static:register
-        DIR=`pwd`;sudo docker run --privileged=true -t --env CI=$CI -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
-        cd /home;
-        sh init_armhf.sh;
-        cp -f ./ci/.dist/bin/Linux_arm_release/v8_test ./arm_root_fs/bin/v8_test;
-        chroot ./arm_root_fs v8_test
-        mv ./arm_root_fs/snapshot-arm-Linux.cc /home/ci/"
-    elif [[ $TARGET_ARCH == "arm64" ]]; then
-        # generate v8-snapshot on arm64 using qemu
-        sudo docker run --rm --privileged multiarch/qemu-user-static:register
-        DIR=`pwd`;sudo docker run --privileged=true -t --env CI=$CI -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
-        cd /home;
-        sh init_arm64.sh;
-        cp -f ./ci/.dist/bin/Linux_arm64_release/v8_test ./arm64_root_fs/bin/v8_test;
-        chroot ./arm64_root_fs v8_test
-        mv ./arm64_root_fs/snapshot-arm64-Linux.cc /home/ci/"
-    elif [[ $TARGET_ARCH == "mips" || $TARGET_ARCH == "mips64" ]]; then
-        # generate v8-snapshot on mips/mips64 using qemu
-        DIR=`pwd`;
-        sudo docker run -t \
-            -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; bash ./arch-run ${TARGET_ARCH} release -e=v8_test;"
-    else
-        .dist/bin/$DIST_DIR/v8_test
-    fi
+    bash ./arch-run ${TARGET_ARCH} ${BUILD_TYPE} -e=v8_test
 else # Darwin/Windows
     .dist/bin/$DIST_DIR/v8_test
 fi
--- .github/workflows/jssdk_test.sh
+++ .github/workflows/jssdk_test.sh
@@ -5,35 +5,7 @@
 set -ev
 
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
-    if [[ $TARGET_ARCH == "arm" ]]; then
-        # run jssdk_test on arm using qemu
-        sudo docker run --rm --privileged multiarch/qemu-user-static:register
-        DIR=`pwd`;sudo docker run --privileged=true -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
-        cd /home;
-        sh init_armhf.sh;
-        cp -f ./ci/.dist/bin/$DIST_DIR/jssdk_test ./arm_root_fs/bin/jssdk_test;
-        chroot ./arm_root_fs jssdk_test"
-    elif [[ $TARGET_ARCH == "arm64" ]]; then
-        # run jssdk_test on arm64 using qemu
-        sudo docker run --rm --privileged multiarch/qemu-user-static:register
-        DIR=`pwd`;sudo docker run --privileged=true -t -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "
-        cd /home;
-        sh init_arm64.sh;
-        cp -f ./ci/.dist/bin/$DIST_DIR/jssdk_test ./arm64_root_fs/bin/jssdk_test;
-        chroot ./arm64_root_fs jssdk_test"
-    elif [[ $TARGET_ARCH == "mips" || $TARGET_ARCH == "mips64" ]]; then
-        # run jssdk_test on mips/mips64 using qemu
-        DIR=`pwd`;
-        sudo docker run -t \
-            -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; bash ./arch-run ${TARGET_ARCH} ${BUILD_TYPE} -e=jssdk_test;"
-    elif [[ $TARGET_ARCH == "i386" ]]; then
-        # run jssdk_test on i386 using qemu
-        DIR=`pwd`;
-        sudo docker run -t \
-            -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; bash ./arch-run ${TARGET_ARCH} ${BUILD_TYPE} -e=jssdk_test;"
-    else
-        .dist/bin/$DIST_DIR/jssdk_test
-    fi
+    bash ./arch-run ${TARGET_ARCH} ${BUILD_TYPE} -e=jssdk_test
 else # Darwin/Windows
     .dist/bin/$DIST_DIR/jssdk_test
 fi
