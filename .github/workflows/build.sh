9c37b3814b1360bda7cc6eccd6d6d1a1220fa0a5
xicilion <lion@9465.net>
2022-11-23 15:03:11 +0000

    build, refactor: specify arch when compile in docker.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,7 @@
 
 if [[ "$BUILD_ALPINE" == "alpine" ]]; then
     CUR=`pwd`
-    docker run --rm -v ${CUR}:/fibjs fibjs/alpine-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${BUILD_TYPE} ci"
+    docker run --rm -v ${CUR}:/fibjs fibjs/alpine-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
 else
     bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci
 fi
