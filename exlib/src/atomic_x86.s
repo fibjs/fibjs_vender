40ae6abca9ccb43f9509c877331d1fcf47cee5a3
xicilion <lion@9465.net>
2022-12-27 11:58:21 +0000

    exlib, feat: atomic_store.

--- exlib/src/atomic_x86.s
+++ exlib/src/atomic_x86.s
@@ -1,5 +1,8 @@
 
-// void __atomic_load(const volatile intptr_t* ptr, intptr_t* ret, memorder);
+// void __atomic_load(size_t size, const void* ptr, void* ret, int memorder);
+
+.globl _atomic_load
+_atomic_load:
 
 .globl __atomic_load
 __atomic_load:
@@ -9,15 +12,84 @@ __atomic_load:
 	movl    16(%esp), %esi
 	movl    20(%esp), %edi
 
+	movl	12(%esp), %ecx
+
+l8:
+	cmpl	$8, %ecx
+	jne		l1
+
 	movq	(%esi), %xmm0
 	movq	%xmm0, (%edi)
+
+	jmp		ln
+
+l1:
+	cmpl	$1, %ecx
+	jne		l2
+
+	movb	(%esi), %al
+	movb	%al, (%edi)
+
+	jmp		ln
+
+l2:
+	cmpl	$2, %ecx
+	jne		l4
+
+	movzwl	(%esi), %eax
+	movw	%ax, (%edi)
+
+	jmp		ln
+
+l4:
+	cmpl	$4, %ecx
+	jne		l8
+
+	movl	(%esi), %eax
+	movl	%eax, (%edi)
+
+	jmp		ln
+
+ln:
+	popl	%esi
+	popl	%edi
+	retl
+
+// void __atomic_store(size_t size, void* ptr, void* val, int memmodel);
+
+.globl _atomic_store
+_atomic_store:
+
+.globl __atomic_store
+__atomic_store:
+	pushl	%edi
+	pushl	%esi
+
+	movl    16(%esp), %edi
+	movl    20(%esp), %esi
+
+	movl	12(%esp), %ecx
+
+	cmpl	$8, %ecx
+	jne		l1
+
+	movq	(%esi), %xmm0
+	movq	%xmm0, (%edi)
+
+	movl	(%esi), %ebx
+	movl	4(%esi), %ecx
+	movl	(%edi), %eax
+	movl	4(%edi), %edx
+sl:
+	lock
+	cmpxchg8b	(%edi)
+	jne	sl
 
 	popl	%esi
 	popl	%edi
 	retl
 
-
-// bool __atomic_compare_exchange (type *ptr, type *expected, type *desired,
+// bool __atomic_compare_exchange (int size, type *ptr, type *expected, type *desired,
 //              bool weak, int success_memorder, int failure_memorder);
 
 .globl __atomic_compare_exchange
--- exlib/test/src/test_atomic.cpp
+++ exlib/test/src/test_atomic.cpp
@@ -1,3 +1,4 @@
+#include "exlib/include/osconfig.h"
 #include "gtest/gtest.h"
 #include <atomic>
 
@@ -32,3 +33,55 @@ TEST(exlib_atomic, compare_exchange)
     double b1 = std::atomic_load(&a);
     GTEST_ASSERT_EQ(b1, 100);
 }
+
+#if defined(i386) && defined(Linux)
+
+extern "C" void _atomic_load(int size, const void* ptr, void* ret, int memorder);
+
+TEST(exlib_atomic, atomic_load)
+{
+    uint64_t a = 0xc0c0c0c0c0c0c0c0;
+    uint64_t b;
+
+    b = 0xffffffffffffffff;
+    _atomic_load(sizeof(int8_t), &a, &b, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xffffffffffffffc0);
+
+    b = 0xffffffffffffffff;
+    _atomic_load(sizeof(int16_t), &a, &b, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xffffffffffffc0c0);
+
+    b = 0xffffffffffffffff;
+    _atomic_load(sizeof(int32_t), &a, &b, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xffffffffc0c0c0c0);
+
+    b = 0xffffffffffffffff;
+    _atomic_load(sizeof(int64_t), &a, &b, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xc0c0c0c0c0c0c0c0);
+}
+
+extern "C" void _atomic_store(size_t size, void* ptr, void* val, int memmodel);
+
+TEST(exlib_atomic, atomic_save)
+{
+    uint64_t a = 0xc0c0c0c0c0c0c0c0;
+    uint64_t b;
+
+    b = 0xffffffffffffffff;
+    _atomic_store(sizeof(int8_t), &b, &a, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xffffffffffffffc0);
+
+    b = 0xffffffffffffffff;
+    _atomic_store(sizeof(int16_t), &b, &a, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xffffffffffffc0c0);
+
+    b = 0xffffffffffffffff;
+    _atomic_store(sizeof(int32_t), &b, &a, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xffffffffc0c0c0c0);
+
+    b = 0xffffffffffffffff;
+    _atomic_store(sizeof(int64_t), &b, &a, __ATOMIC_RELAXED);
+    GTEST_ASSERT_EQ(b, 0xc0c0c0c0c0c0c0c0);
+}
+
+#endif
