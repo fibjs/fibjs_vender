77191cc34d886e6ef628bbe6882fca14bfab3c5b
xicilion <lion@9465.net>
2023-01-10 14:18:18 +0800

    build, refactor: change ci to docker.

--- .github/workflows/jssdk_test.sh
+++ .github/workflows/jssdk_test.sh
@@ -5,12 +5,12 @@
 set -ev
 
 if [[ $TARGET_OS_NAME == 'Linux' ]]; then
-    if [[ "$BUILD_TARGET" != "" ]]; then
-        CUR=`pwd`
-        docker run --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} /fibjs/.dist/bin/${DIST_DIR}/jssdk_test
-    else
-        bash ./arch-run ${TARGET_ARCH} ${BUILD_TYPE} -e=jssdk_test
+    if [[ "$BUILD_TARGET" == "" ]]; then
+        BUILD_TARGE="linux"
     fi
+
+    CUR=`pwd`
+    docker run --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-test-env:${TARGET_ARCH} /fibjs/.dist/bin/${DIST_DIR}/jssdk_test
 else # Darwin/Windows
     .dist/bin/$DIST_DIR/jssdk_test
 fi
