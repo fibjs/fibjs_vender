dd6e6246e00a326c9bbba785218fd0d58b8a02c3
xicilion <lion@9465.net>
2023-01-18 02:25:38 +0800

    build, bugfix: build.sh syntax error.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,6 +5,7 @@ TARGET_OS=`uname`
 if [[ "$TARGET_OS" == "Linux" ]]; then
     if [[ "$BUILD_TARGET" == "" ]]; then
         BUILD_TARGET="linux"
+    fi
 
     CUR=`pwd`
     docker run -t --rm -v ${CUR}:/fibjs fibjs/${BUILD_TARGET}-build-env:${TARGET_ARCH} bash -c "cd /fibjs; bash build -j2 ${TARGET_ARCH} ${BUILD_TYPE} ci"
