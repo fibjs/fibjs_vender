eb7524994e936a40066c62c763b986ba60032d7e
xicilion <lion@9465.net>
2023-02-02 02:12:30 +0800

    build, refactor: remove out/bin directly in `build clean`.

--- build.cmake
+++ build.cmake
@@ -1,50 +1,3 @@
-include(build_tools/cmake-scripts/get_env.cmake)
-
-set(libs
-    exlib
-    cares
-    gtest
-    unicode
-    expat
-    gumbo
-    gd
-    tiff
-    jpeg
-    png
-    webp
-    jemalloc
-    zlib
-    leveldb
-    snappy
-    msgpack
-    pcre
-    sqlite
-    mongo
-    umysql
-    uuid
-    exif
-    mbedtls
-    secp256k1
-    curve25519
-    blst
-    blake2
-    v8
-    jssdk
-    unzip
-    uv
-)
-
-if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-    list(APPEND libs
-        winiconv
-    )
-else()
-    list(APPEND libs
-        editline
-        ev
-    )
-endif()
-
 set(WORK_ROOT $ENV{WORK_ROOT})
 if(NOT WORK_ROOT)
     if(EXISTS "../build")
@@ -55,9 +8,56 @@ if(NOT WORK_ROOT)
 endif()
 
 if("${CLEAN_BUILD}" STREQUAL "true")
-    rimraf(${WORK_ROOT}/out)
-    rimraf(${WORK_ROOT}/bin)
+    file(REMOVE_RECURSE ${WORK_ROOT}/out)
+    file(REMOVE_RECURSE ${WORK_ROOT}/bin)
 else()
+    include(build_tools/cmake-scripts/get_env.cmake)
+
+    set(libs
+        exlib
+        cares
+        gtest
+        unicode
+        expat
+        gumbo
+        gd
+        tiff
+        jpeg
+        png
+        webp
+        jemalloc
+        zlib
+        leveldb
+        snappy
+        msgpack
+        pcre
+        sqlite
+        mongo
+        umysql
+        uuid
+        exif
+        mbedtls
+        secp256k1
+        curve25519
+        blst
+        blake2
+        v8
+        jssdk
+        unzip
+        uv
+    )
+
+    if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+        list(APPEND libs
+            winiconv
+        )
+    else()
+        list(APPEND libs
+            editline
+            ev
+        )
+    endif()
+
     foreach(lib ${libs})
         build("${CMAKE_CURRENT_SOURCE_DIR}/${lib}" "${WORK_ROOT}" "${lib}")
     endforeach()
