4a10785e924d7d68078a6ad313acd26ed3eee9c6
xicilion <lion@9465.net>
2023-08-24 01:31:06 +0800

    blst, refactor: update BLS12_381_G1_P1 for bbs signature.

--- blst/include/bbs.h
+++ blst/include/bbs.h
@@ -8,7 +8,6 @@ extern "C" {
 #endif
 
 extern const blst_p1_affine BLS12_381_G1_P1;
-void blst_hash_to_scalar(blst_scalar* out, const byte* msg, size_t msg_len, const byte* DST, size_t DST_len);
 
 #ifdef __cplusplus
 }
--- blst/src/bbs_server.c
+++ blst/src/bbs_server.c
@@ -7,23 +7,10 @@
 #define FR_BYTE_SIZE 48
 
 const POINTonE1 BLS12_381_G1_P1 = {
-    { TO_LIMB_T(0x0116646f7d850908), TO_LIMB_T(0xdbe7a7e23818b142),
-        TO_LIMB_T(0xa6107c68d122c08e), TO_LIMB_T(0xb89d2d7c6795ab48),
-        TO_LIMB_T(0x9ce9f90855c67e48), TO_LIMB_T(0x1019697ce9c4f52b) },
-    { TO_LIMB_T(0xe8999ae376cb397c), TO_LIMB_T(0x3a3db0c5254c808b),
-        TO_LIMB_T(0xd4459bc183f8a58e), TO_LIMB_T(0x26220a7862b7c9c4),
-        TO_LIMB_T(0xa625433d10cc25a6), TO_LIMB_T(0x16597689fda005c5) }
+    { TO_LIMB_T(0xcbbca09048aca92e), TO_LIMB_T(0x44035f6216317fd5),
+        TO_LIMB_T(0x95388041e9218d1b), TO_LIMB_T(0xe4b58b6b099a3bc8),
+        TO_LIMB_T(0x58e6116ffcc2ab08), TO_LIMB_T(0x0d5288df7345fa9e) },
+    { TO_LIMB_T(0x886572435ff05797), TO_LIMB_T(0xa66395c7d2bb0e58),
+        TO_LIMB_T(0xb104b28548a7bc46), TO_LIMB_T(0x4f5ebbc008cb6cf1),
+        TO_LIMB_T(0xea5bd123cf3662e1), TO_LIMB_T(0x167b4ad247d7ec89) }
 };
-
-void blst_hash_to_scalar(pow256* out, const byte* msg, size_t msg_len,
-    const byte* DST, size_t DST_len)
-{
-    byte buf[64];
-    int counter = 0;
-
-    do {
-        expand_message_xmd(buf, FR_BYTE_SIZE, msg, msg_len,
-            (const unsigned char*)&counter, 1, DST, DST_len);
-        counter++;
-    } while (!blst_scalar_from_be_bytes((unsigned char*)out, buf, FR_BYTE_SIZE));
-}
