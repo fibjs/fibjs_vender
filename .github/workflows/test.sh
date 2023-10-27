59273e56708ad23e71e7752a6eac347cbd460187
xicilion <lion@9465.net>
2023-10-27 23:03:04 +0800

    build, refactor: docker name for linux.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,14 +6,14 @@ DIST_EXEC="bin/${HOST_OS}_${BUILD_ARCH}_${BUILD_TYPE}/jssdk_test"
 if [[ $HOST_OS == 'Linux' ]]; then
     CUR=`pwd`
 
-    if [[ "$BUILD_TARGET" != "" ]]; then
-        docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-test-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
-    else
+    if [[ "$BUILD_TARGET" == "linux" ]]; then
         if [[ $BUILD_ARCH == 'x64' ]]; then
             docker run -t --rm -v ${CUR}:${CUR} ubuntu:12.04 bash -c "cd ${CUR}; $DIST_EXEC"
         else
-            docker run -t --rm -v ${CUR}:${CUR} fibjs/linux-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
+            docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
         fi
+    else
+        docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-test-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
     fi
 else # Windows/Darwin
     $DIST_EXEC
