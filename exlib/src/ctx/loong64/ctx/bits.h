6d6244122bc7e2975da7a2f45dd4f15290bd2391
xicilion <lion@9465.net>
2024-01-24 13:22:05 +0800

    exlib, refactor: update ctx_ucontext struct in bits.h for loong64.

--- exlib/src/ctx/loong64/ctx/bits.h
+++ exlib/src/ctx/loong64/ctx/bits.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <bits/types/sigset_t.h>
-#include <bits/types/stack_t.h>
-
 typedef struct {
     unsigned long long __pc;
     unsigned long long __gregs[32];
@@ -10,9 +7,15 @@ typedef struct {
     unsigned long long __fpregs[8] __attribute__((__aligned__(16)));
 } ctx_mcontext_t;
 
+typedef struct {
+    void* ss_sp;
+    size_t ss_size;
+    int ss_flags;
+} ctx_stack_t;
+
 typedef struct ctx_ucontext {
     unsigned long uc_flags;
     struct ctx_ucontext* uc_link;
-    stack_t uc_stack;
+    ctx_stack_t uc_stack;
     ctx_mcontext_t uc_mcontext;
 } ctx_ucontext_t;
