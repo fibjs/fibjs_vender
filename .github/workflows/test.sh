48d224454c07dde2acfadfa4c966c659f80cc9e6
xicilion <lion@9465.net>
2024-02-07 01:16:34 +0800

    build, refactor: refactor build dockerfile.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,11 +6,7 @@ DIST_EXEC="bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/jssdk_test"
 if [[ $HOST_OS == 'Linux' ]]; then
     CUR=`pwd`
 
-    if [[ "$BUILD_TARGET" == "linux" ]]; then
-        docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
-    else
-        docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-test-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
-    fi
+    docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
 else # Windows/Darwin
     $DIST_EXEC
 fi
