9ba8d61a75410372555b3ea92bb145b5fb46b602
xicilion <lion@9465.net>
2025-12-13 17:00:44 +0000

    build: update docker run command to include --privileged flag and remove test skip

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,7 +6,7 @@ DIST_EXEC="bin/${BUILD_OS}_${BUILD_ARCH}_${BUILD_TYPE}/jssdk_test"
 if [[ $HOST_OS == 'Linux' ]]; then
     CUR=`pwd`
 
-    docker run -t --rm -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
+    docker run -t --rm --privileged -v ${CUR}:${CUR} fibjs/${BUILD_TARGET}-build-env:${BUILD_ARCH} bash -c "cd ${CUR}; $DIST_EXEC"
 else # Windows/Darwin
     $DIST_EXEC
 fi
